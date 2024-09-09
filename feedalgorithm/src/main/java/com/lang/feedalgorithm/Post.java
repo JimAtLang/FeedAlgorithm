01d12f49699d3d23b7347b63f1a3f764a2e4d139
jimatlang <jim.stewart@thelangschool.org>
2024-09-09 09:43:49 -0400

    timeStamp property of Post

--- feedalgorithm/src/main/java/com/lang/feedalgorithm/Post.java
+++ feedalgorithm/src/main/java/com/lang/feedalgorithm/Post.java
@@ -1,14 +1,22 @@
 package com.lang.feedalgorithm;
 
+import java.util.Date;
+
 public class Post {
     private String title;
     private String text;
     private User author;
+    private Date timeStamp;
+
+    public Date getTimeStamp() {
+        return timeStamp;
+    }
 
     public Post(String title, String text, User author) {
         this.title = title;
         this.text = text;
         this.author = author;
+        this.timeStamp = new Date();
     }
 
     public User getAuthor() {
