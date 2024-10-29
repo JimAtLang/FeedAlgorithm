a56b122110d1a7b809354e98a34bd40c1dbd5366
us riverside <us.riverside@thelangschool.net>
2024-10-29 15:01:00 -0400

    created feebuilder really

--- feedalgorithm/src/main/java/com/lang/feedalgorithm/FeedBuilder.java
+++ feedalgorithm/src/main/java/com/lang/feedalgorithm/FeedBuilder.java
@@ -1,5 +1,9 @@
 package com.lang.feedalgorithm;
 
 public class FeedBuilder {
-
+    public Feed chronologicalFeed(User user){
+        Feed f = new Feed();
+        // we need to build the feed in here
+        return f;
+    }
 }
